3964db81fe73d63aa00438d8510d1d101c2affd9
marlene weiss <marlene.weiss@stud.uibk.ac.at>
2018-05-14 15:32:33 +0200

    Anmerkungen eingefügt

--- biketirol/biketirol.js
+++ biketirol/biketirol.js
@@ -1,9 +1,11 @@
-// Start- und Endpunkte der Route als Marker mit Popup, Namen, Wikipedia Link und passenden Icons für Start/Ziel von https://mapicons.mapsmarker.com/ 
+/*
+Auf Reihenfolge achten (bei Error: XY is not defined: Variablen (let/const) müssen immer vor Aufruf definiert werden
+L. verweist auf Leaflet-Dokumentation, gelb geschriebenes ruft dort hinterlegte Befehle ab
+myMap.addLayer(), nicht vergessen
+*/
 
-// Overlay controls zum unabhängigem Ein-/Ausschalten der Route und Marker hinzufügen
-
-let myMap = L.map("map");
-let etappeGroup = L.featureGroup();
+let myMap = L.map("map"); // ! Abgleichen mit Name in html-File <div id="map"></div>
+let etappeGroup = L.featureGroup(); // =^Marker-Gruppe
 let myLayers = {
     osm: L.tileLayer(
         "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
@@ -37,7 +39,7 @@ let myLayers = {
             attribution: "Datenquelle: <a href='https://www.data.gv.at/katalog/dataset/land-tirol_elektronischekartetirol'>elektronische Karte Tirol Orthophoto</a>"
         }
     ),
-    eKartebeschriftung: L.tileLayer(
+    eKartebeschriftung: L.tileLayer( // Ortsnamen
         "http://wmts.kartetirol.at/wmts/gdi_nomenklatur/GoogleMapsCompatible/{z}/{x}/{y}.png8",
         {
             attribution: "Datenquelle: <a href='https://www.data.gv.at/katalog/dataset/land-tirol_elektronischekartetirol/resource/062d55d0-4533-4d3f-b507-106bb44285b7'>elektronische Karte Tirol</a>"
@@ -46,7 +48,7 @@ let myLayers = {
 
 };
 
-let eKartesommergruppe = L.featureGroup(
+let eKartesommergruppe = L.featureGroup( // fügt Basemaps zusammen: zum einen Orthophoto, zum anderen zugehörige Beschriftung
     [
         myLayers.eKartesommer,
         myLayers.eKartebeschriftung,
@@ -65,17 +67,15 @@ let eKarteorthogruppe = L.featureGroup(
     ]
 );
 
-myMap.addLayer(myLayers.osm);
+myMap.addLayer(myLayers.osm); // Website startet mit eKartesommergruppe (Sommerfoto inkl Beschriftung)
 
-
-
-const geojsonLayer = L.geoJSON (geojson, {
+const geojsonLayer = L.geoJSON (geojson, { //Einbindung der gpx-Datei, die als const geojson in eigener Datei (etappe31.geojson.js) definiert wurde. geojsonLayer ist die Variable, die hier definiert wird, geoJSON der Leaflet-Befehl, geojson bezieht sich auf die const geojson aus der etappe31.geojson.js-Datei
     style: function (feature) {
         return { color: "#ff0000" }}
 });
 myMap.addLayer(geojsonLayer);
 
-let myMapControl = L.control.layers ({
+let myMapControl = L.control.layers ({ // muss unterhalb der ganzen Variablen stehen (!!geojsonLayer)
 
     "Openstreetmap": myLayers.osm,
     "basemap.at Gundkarte": myLayers.geolandbasemap,
@@ -106,21 +106,9 @@ myMap.addLayer(etappeGroup);
 const start = [47.123786, 10.247623]
 const finish = [47.241426, 10.292558]
 
-L.marker(start).addTo(etappeGroup).bindPopup("<p>Startpunkt</p><a href='https://de.wikipedia.org/wiki/St._Anton_am_Arlberg'>Wikipedia Link</a>");
+L.marker(start).addTo(etappeGroup).bindPopup("<p>Startpunkt</p><a href='https://de.wikipedia.org/wiki/St._Anton_am_Arlberg'>Wikipedia Link</a>"); //Marker-Definition, durch bondPopup ist das Popup direkt dran
 
 L.marker(finish).addTo(etappeGroup).bindPopup("<p>Endpunkt</p><a href='https://de.wikipedia.org/wiki/Steeg_(Tirol)'>Wikipedia Link</a>");
 
-
-//L.marker(start).addTo(etappeGroup);
-//L.marker(finish).addTo(etappeGroup);
-
 myMap.fitBounds(etappeGroup.getBounds());
-
-
-//myMap.fitBounds(etappeGroup.getBounds()); 
-
-/*const url = "https://data.wien.gv.at/daten/geo?service=WFS&request=GetFeature&version=1.1.0&srsName=EPSG:4326&outputFormat=json&typeName=ogdwien:SPAZIERPUNKTOGD,ogdwien:SPAZIERLINIEOGD";
-
-addGeojson(url);
-*/
 
