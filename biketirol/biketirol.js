8a235dcc1121c4ce7696547c1f646c2fb05f9e93
marlene weiss <marlene.weiss@stud.uibk.ac.at>
2018-05-14 14:40:07 +0200

    Karten, Route, Marker da

--- biketirol/biketirol.js
+++ biketirol/biketirol.js
@@ -17,17 +17,18 @@
 // WMTS URLs siehe https://www.data.gv.at/katalog/dataset/land-tirol_elektronischekartetirol
 
 // Maßstab metrisch ohne inch
-
+//>>>
 // Start- und Endpunkte der Route als Marker mit Popup, Namen, Wikipedia Link und passenden Icons für Start/Ziel von https://mapicons.mapsmarker.com/ >> Punkt über Koordinaten definieren?
 
-// GeoJSON Track als Linie in der Karte einzeichnen und auf Ausschnitt zoomen >> nee, aus gpx > wie wien stadtspaziergang einbinden
+// GeoJSON Track als Linie in der Karte einzeichnen und auf Ausschnitt zoomen >> aus gpx > wie wien stadtspaziergang einbinden
 // Einbauen nicht über async, sondern über ein L.geoJSON() mit einem Javascript Objekt (wie beim ersten Stadtspaziergang Wien Beispiel)
 
 // Baselayer control für OSM, basemap.at, Elektronische Karte Tirol hinzufügen
 
 // Overlay controls zum unabhängigem Ein-/Ausschalten der Route und Marker hinzufügen
 
-let myMap = L.map("mapdiv");
+let myMap = L.map("map");
+let etappeGroup = L.featureGroup();
 let myLayers = {
     osm: L.tileLayer(
         "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
@@ -43,39 +44,33 @@ let myLayers = {
             attribution: "Datenquelle: <a href='https://www.basemap.at'>basemap.at</a>"
         }
     ),
-    bmaporthofoto30cm: L.tileLayer(
-        "https://{s}.wien.gv.at/basemap/bmaporthofoto30cm/normal/google3857/{z}/{y}/{x}.jpeg",
-        {
-            subdomains: ["maps", "maps1", "maps2", "maps3", "maps4"],
-            attribution: "Datenquelle: <a href='https://www.basemap.at'>basemap.at</a>"
-        }
-    ),
-    
     eKartesommer: L.tileLayer(
-        "http://wmts.kartetirol.at/wmts/gdi_base_summer/{TileMatrixSet}/{TileMatrix}/{TileCol}/{TileRow}.jpeg80",
+        "http://wmts.kartetirol.at/wmts/gdi_base_summer/GoogleMapsCompatible/{z}/{x}/{y}.jpeg80",
         {
             attribution: "Datenquelle: <a href='https://www.data.gv.at/katalog/dataset/land-tirol_elektronischekartetirol'>elektronische Karte Tirol Sommer</a>"
         }
     ),
     eKartewinter: L.tileLayer(
-        "http://wmts.kartetirol.at/wmts/gdi_base_summer/{TileMatrixSet}/{TileMatrix}/{TileCol}/{TileRow}.jpeg80",
+        "http://wmts.kartetirol.at/wmts/gdi_base_summer/GoogleMapsCompatible/{z}/{x}/{y}.jpeg80",
         {
             attribution: "Datenquelle: <a href='http://wmts.kartetirol.at/wmts/gdi_base_winter/{TileMatrixSet}/{TileMatrix}/{TileCol}/{TileRow}.jpeg80'>elektronische Karte Tirol Winter</a>"
         }
     ),
     eKarteorthofoto: L.tileLayer(
-        "http://wmts.kartetirol.at/wmts/gdi_ortho/{TileMatrixSet}/{TileMatrix}/{TileCol}/{TileRow}.jpeg80",
+        "http://wmts.kartetirol.at/wmts/gdi_ortho/GoogleMapsCompatible/{z}/{x}/{y}.jpeg80",
         {
             attribution: "Datenquelle: <a href='https://www.data.gv.at/katalog/dataset/land-tirol_elektronischekartetirol'>elektronische Karte Tirol Orthophoto</a>"
         }
     ),
     eKartebeschriftung: L.tileLayer(
-        "http://wmts.kartetirol.at/wmts/gdi_nomenklatur/{TileMatrixSet}/{TileMatrix}/{TileCol}/{TileRow}.png8",
+        "http://wmts.kartetirol.at/wmts/gdi_nomenklatur/GoogleMapsCompatible/{z}/{x}/{y}.png8",
         {
             attribution: "Datenquelle: <a href='https://www.data.gv.at/katalog/dataset/land-tirol_elektronischekartetirol/resource/062d55d0-4533-4d3f-b507-106bb44285b7'>elektronische Karte Tirol</a>"
         }
-    )
+    ),
+
 };
+
 let eKartesommergruppe = L.featureGroup(
     [
         myLayers.eKartesommer,
@@ -97,21 +92,30 @@ let eKarteorthogruppe = L.featureGroup(
 
 myMap.addLayer(myLayers.osm);
 
-let myMapControl = L.control.layers({
+let myMapControl = L.control.layers ({
 
     "Openstreetmap": myLayers.osm,
     "basemap.at Gundkarte": myLayers.geolandbasemap,
-    "basemap.at Orthofoto": myLayers.bmaporthofoto30cm,
     "eKarte Tirol Sommer": eKartesommergruppe,
     "eKarte Tirol Winter": eKartewintergruppe,
-    "eKarte Tirol Sommer": eKarteorthogruppe,
-
+    "eKarte Tirol Ortho": eKarteorthogruppe,
+   // "basemap.at Orthofoto": myLayers.bmaporthofoto30cm,
 },
-
-    { "collapsed": false });
+{   
+   "Start- & Endpunkte" : etappeGroup,
+   //"Route" : geojson,
+},
+{
+collapsed: false 
+});
 
 myMap.addControl(myMapControl);
 
+const geojsonLayer = L.geoJSON (geojson, {
+    style: function (feature) {
+        return { color: "#ff0000" }}
+});
+myMap.addLayer(geojsonLayer);
 L.control.scale({
     maxWidth: 200,
     metric: true,
@@ -119,4 +123,22 @@ L.control.scale({
     position: "bottomleft",
 }).addTo(myMap);
 
-//myMap.fitBounds(linie.getBounds()); 
+//etappeGroup.addLayer(geojson);
+
+myMap.addLayer(etappeGroup);
+const start = [47.123786, 10.247623]
+const finish = [47.241426, 10.292558]
+
+L.marker(start).addTo(etappeGroup);
+L.marker(finish).addTo(etappeGroup);
+
+myMap.fitBounds(etappeGroup.getBounds());
+
+
+//myMap.fitBounds(etappeGroup.getBounds()); 
+
+/*const url = "https://data.wien.gv.at/daten/geo?service=WFS&request=GetFeature&version=1.1.0&srsName=EPSG:4326&outputFormat=json&typeName=ogdwien:SPAZIERPUNKTOGD,ogdwien:SPAZIERLINIEOGD";
+
+addGeojson(url);
+*/
+
--- biketirol/etappe31.geojson.js
+++ biketirol/etappe31.geojson.js
@@ -1,4 +1,4 @@
-const gpx =
+const geojson =
 {
     "type": "FeatureCollection",
     "features": [
